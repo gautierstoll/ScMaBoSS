f2e339ff886b66412247b8caaa6722b167986a33
Gautier Stoll <gautier.stoll@curie.fr>
2019-10-10 18:22:13 +0200

    UPMaBoSS, final, partially tested

--- UPMaBoSS.scala
+++ UPMaBoSS.scala
@@ -1,8 +1,9 @@
 import org.apache.commons.lang.ObjectUtils.Null
 import scalatags.Text.short.*
-
 import java.io._
+
 import scala.util.Random
+import scala.util.matching.Regex
 
 
 object UPMaBoSS {
@@ -40,21 +41,25 @@ object UPMaBoSS {
     * @param upProb
     * @return
     */
-  def upProbFromInitCond(initCondProb : List[(String,Double)] , upProb : String) : Double = {
+  def upProbFromInitCond(initCondProb : List[(String,Double)] , upProb : String,hex :Boolean = false) : String = {
+    println("upProb: "+upProb)
     val nodes = try (upProb.split("=").head) catch
       {case _:Throwable => throw new IllegalArgumentException("cannot parse "+upProb)}
     val boolState = try (upProb.split("=").tail.head) catch
       {case _:Throwable => throw new IllegalArgumentException("cannot parse "+upProb)}
     val nodeList = "\\s*\\)\\s*".r.replaceAllIn("p\\[\\s*\\(\\s*".r.replaceAllIn(nodes,""),"").split(",").
       map(x=> "\\s*".r.replaceAllIn(x,"")).toList
+    println("nodeList: "+nodeList)
     val boolStateList : List[Boolean] = "\\s*\\)\\s*\\]".r.replaceAllIn("\\s*\\(\\s*".r.replaceAllIn(boolState,""),"").split(",").
       map(x=> if("\\s*".r.replaceAllIn(x,"") == "1") true else false).toList
+    println("boolStateList: "+boolStateList)
     val activeNodes = nodeList.zip(boolStateList).filter(_._2).map(_._1).toSet
     val inactiveNodes = nodeList.zip(boolStateList).filter(!_._2).map(_._1).toSet
-    initCondProb.filter(prob => {
+    val probOut = initCondProb.filter(prob => {
       val activeProbNodes = prob._1.split(" -- ").toSet
-      activeProbNodes.subsetOf(activeNodes) & (activeProbNodes.intersect(inactiveNodes)).size == 0
+      activeNodes.subsetOf(activeProbNodes) & activeProbNodes.intersect(inactiveNodes).isEmpty
     }).map(_._2).sum
+    if (hex) java.lang.Double.toHexString(probOut) else probOut.toString
   }
 }
 
@@ -80,43 +85,57 @@ class UPMaBoSS(val divNode : String, val deathNode : String, val updateVar : Lis
     pw.write("seed = "+seed.toString+"\n")
     pw.close
   }
-  val updateVarNames : List[String] = updateVar.map(x => "\\s*".r.replaceAllIn("=.*".r.replaceAllIn(x,""),""))
+  val updateVarNames : List[String] = updateVar.map(x => "\\s*".r.replaceAllIn("u=.*".r.replaceAllIn(x,""),""))
   val upRandom: Random = new Random(seed)
   val hints : Hints = Hints(check = false,hexfloat = hexUP,augment = true,overRide = false,verbose = false)
-  case class UpStep(cfgMbss: CfgMbss, result: Result = null, relSize : Double = 1d) {}
+
   def setUpdateVar(probDistRelSize : (List[(String,Double)],Double)) : String = {
-    def replaceRand(s:String) : String = {
+    def recReplaceRand(s:String) : String = {
       "#rand".r.findFirstIn(s) match {
         case None => s
         case Some(c) =>
-          replaceRand("#rand".r.replaceFirstIn(s,
+          recReplaceRand("#rand".r.replaceFirstIn(s,
             if (hexUP) java.lang.Double.toHexString(upRandom.nextDouble()) else upRandom.nextDouble().toString))
       }
     }
     val updateVarProb = updateVar.map(line => {
-    "(p\\[[^\\]+]\\])".r.replaceAllIn(line,
-      if (hexUP) java.lang.Double.toHexString(UPMaBoSS.upProbFromInitCond(probDistRelSize._1,"$1"))
-    else UPMaBoSS.upProbFromInitCond(probDistRelSize._1,"$1").toString)}).mkString("\n")
+      val listReplaceProb : List[String] =
+        "p\\[[^\\]]+\\]".r.findAllIn(line).map(x=>UPMaBoSS.upProbFromInitCond(probDistRelSize._1,x,hexUP)).toList
+      println("Replace strings: "+listReplaceProb)
+      def recReplace(regex: Regex,s:String,lReplace : List[String]):String = {
+        lReplace match {
+          case Nil => s
+          case l => recReplace(regex, regex.replaceFirstIn(s, l.head), l.tail)
+        }
+      }
+        recReplace("p\\[[^\\]]+\\]".r,line,listReplaceProb)
+      }).map(x=>"u=".r.replaceAllIn(x,"=")).mkString("\n")
     val updatePopRatio = "#pop_ratio".r.replaceAllIn(updateVarProb,if (hexUP) java.lang.Double.toHexString(probDistRelSize._2) else probDistRelSize._2.toString)
-    replaceRand(updatePopRatio)
+    recReplaceRand(updatePopRatio)
   }
 
+  case class UpStep(cfgMbss: CfgMbss, result: Result = null, relSize : Double = 1d) {}
 
   def upDate(upStep : UpStep) : UpStep = {
-    val mcli = new MaBoSSClient(port = 4291)
-    val newResult = mcli.run(upStep.cfgMbss, hints)
-    mcli.close()
-    val newInitCond = newResult.updateLastLine(divNode, deathNode)
-    val newRelSize = upStep.relSize * newInitCond._2
-    val newInitCondCfg = upStep.cfgMbss.setInitCond(newInitCond._1.map(x => (new NetState(x._1, cfgMbss), x._2)),hex = hexUP)
-    val newCfgString = updateVarNames match {
-      case Nil => newInitCondCfg.cfg + "\n" +
-        setUpdateVar(newInitCond)
-      case l => newInitCondCfg.cfg.split("\n").filter(x => !(updateVarNames.map(name => name.r.findFirstIn(x).isDefined).reduce(_ | _))).mkString("\n") + "\n" +
-        setUpdateVar(newInitCond)
+    if (upStep.relSize == 0) UpStep(null,null,0) else {
+      val mcli = new MaBoSSClient(port = 4291)
+      println("Run MaBoSS with "+upStep.cfgMbss.cfg)
+      val newResult = mcli.run(upStep.cfgMbss, hints)
+      mcli.close()
+      println("last line: "+newResult.parsedResultData.prob_traj.split("\n").toList.last)
+      val newInitCond = newResult.updateLastLine(divNode, deathNode)
+      println("newInitCond: "+newInitCond._1)
+      val newRelSize = upStep.relSize * newInitCond._2
+      val newInitCondCfg = upStep.cfgMbss.setInitCond(newInitCond._1.map(x => (new NetState(x._1, cfgMbss), x._2)), hex = hexUP)
+      val newCfgString = updateVarNames match {
+        case Nil => newInitCondCfg.cfg + "\n" +
+          setUpdateVar(newInitCond)
+        case l => newInitCondCfg.cfg.split("\n").filter(x => !(updateVarNames.map(name => ("\\"+name+"\\s*=").r.findFirstIn(x).isDefined).reduce(_ | _))).mkString("\n") + "\n" +
+          setUpdateVar(newInitCond)
+      }
+      val newCfg = new CfgMbss(newInitCondCfg.bndMbss, newCfgString)
+      UpStep(newCfg, newResult, newRelSize)
     }
-    val newCfg = new CfgMbss(newInitCondCfg.bndMbss,newCfgString)
-    UpStep(newCfg, newResult, newRelSize)
   }
 
   val strRun : Stream[UpStep] = UpStep(cfgMbss) #:: strRun.map(res => upDate(res))
@@ -134,31 +153,33 @@ class UPMaBoSS(val divNode : String, val deathNode : String, val updateVar : Lis
   case class UpStepLight(lastLineProbTraj : Option[String] = None, relSize : Double = 1d) {}
 
   def upDateLight(upStep : UpStepLight) : UpStepLight = {
-    val newInitCond = upStep.lastLineProbTraj match {
-      case None => None
-      case Some(line) => Some(Result.updateLine(line,divNode, deathNode))
-    }
-    val newRelSize = newInitCond  match {
-      case None => upStep.relSize
-      case Some((dist,ratio)) => upStep.relSize * ratio
-    }
-    val newCfg = newInitCond match {
-      case None => cfgMbss
-      case Some((dist,ratio)) => {
-        val newInitCondCfg = cfgMbss.setInitCond(dist.map(x => (new NetState(x._1, cfgMbss), x._2)), hex = hexUP)
-        val newCfgString = updateVarNames match {
-          case Nil => newInitCondCfg.cfg + "\n" +
-            setUpdateVar((dist, ratio))
-          case l => newInitCondCfg.cfg.split("\n").filter(x => !(updateVarNames.map(name => name.r.findFirstIn(x).isDefined).reduce(_ | _))).mkString("\n") + "\n" +
-            setUpdateVar((dist, ratio))
-        }
-        new CfgMbss(newInitCondCfg.bndMbss, newCfgString)
+    if (upStep.relSize == 0) UpStepLight(None,0) else {
+      val newInitCond = upStep.lastLineProbTraj match {
+        case None => None
+        case Some(line) => Some(Result.updateLine(line, divNode, deathNode))
       }
+      val newRelSize = newInitCond match {
+        case None => upStep.relSize
+        case Some((dist, ratio)) => upStep.relSize * ratio
+      }
+      val newCfg = newInitCond match {
+        case None => cfgMbss
+        case Some((dist, ratio)) => {
+          val newInitCondCfg = cfgMbss.setInitCond(dist.map(x => (new NetState(x._1, cfgMbss), x._2)), hex = hexUP)
+          val newCfgString = updateVarNames match {
+            case Nil => newInitCondCfg.cfg + "\n" +
+              setUpdateVar((dist, ratio))
+            case l => newInitCondCfg.cfg.split("\n").filter(x => !(updateVarNames.map(name => name.r.findFirstIn(x).isDefined).reduce(_ | _))).mkString("\n") + "\n" +
+              setUpdateVar((dist, ratio))
+          }
+          new CfgMbss(newInitCondCfg.bndMbss, newCfgString)
+        }
+      }
+      val mcli = new MaBoSSClient(port = 4291)
+      val result = mcli.run(newCfg, hints)
+      mcli.close()
+      UpStepLight(Some(result.parsedResultData.prob_traj.split("\n").toList.last), newRelSize)
     }
-    val mcli = new MaBoSSClient(port = 4291)
-    val result = mcli.run(newCfg, hints)
-    mcli.close()
-    UpStepLight(Some(result.parsedResultData.prob_traj.split("\n").toList.last),newRelSize)
   }
 
   val strRunLight : Stream[UpStepLight] = UpStepLight() #:: strRunLight.map(res => upDateLight(res))//careful, simulation data start at index 1
