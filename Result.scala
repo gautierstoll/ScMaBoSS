58c5446d7306011d07504331384ec7f32aac0a0c
gautierstoll <gautier.stoll@gmail.com>
2020-04-14 10:40:12 +0200

    small error in probTrajLine2Dist

--- Result.scala
+++ Result.scala
@@ -365,6 +365,6 @@ trait ResultProcessing {
   def probTrajLine2Dist(index: Int,simulation: CfgMbss): Array[(NetState,Double)]  = {
     linesWithTime(index).split("\t").
     dropWhile("^[0-9].*".r.findFirstIn(_).isDefined).sliding(3, 3).
-      map(x => (new NetState(x(0),simulation), x(1).toDouble))
+      map(x => (new NetState(x(0),simulation), x(1).toDouble)).toArray
   }
 }
