be83a98461f70e87855faef010bd7e515c44c4ba
Gautier Stoll <gautier.stoll@curie.fr>
2022-03-22 15:50:01 +0100

    Tested PopMaBoSS classes

--- InputPopMBSS.scala
+++ InputPopMBSS.scala
@@ -2,14 +2,27 @@ package ScMaBoSS
 
 import scala.collection.immutable.List
 
-
+/** Class for population state, for PopMaBoSS
+  *
+  * @param stateString string representation of population state
+  * @param nodeList list of model nodes, necessary for constructing the set of NetStates
+  */
 class PopNetState(val stateString: String,nodeList : List[String]){
  val state : Map[NetState,Long] = stateString.replaceAll("\\s*\\[|\\]\\s*","").
    split(",").map( s => {val keyVal = s.replaceAll("\\{|\\}","").split(":")
    (new NetState(keyVal(0),nodeList),keyVal(1).toLong)}).toMap
 }
 
+/** Sub class of BndMbss, for PopMaBoSS
+  *
+  * @param pbnd
+  */
 class PBndMBSS(val pbnd : String) extends BndMbss(pbnd)
 
+/** sub class of CfgMbss, for PopMaBoSS
+  *
+  * @param pbnd
+  * @param pcfg
+  */
 class PCfgMBSS(val pbnd : PBndMBSS,val pcfg : String) extends CfgMbss(pbnd,pcfg)
 
--- PopResult.scala
+++ PopResult.scala
@@ -1,8 +1,41 @@
 package ScMaBoSS
 
+import java.sql.Time
+
 import scala.collection.immutable.List
 
-class PResultFromFile(filenamePop: String,filenameSimplPopFile:String,val listNodes: List[String]){
+/** Parsed results of PopMaBoSS
+  *
+  * @param filenamePop
+  * @param filenameSimplPop
+  * @param listNodes
+  */
+class PResultFromFile(filenamePop: String,filenameSimplPop:String,val listNodes: List[String]){
+  private val splittedSimpleFile: List[String] = ManageInputFile.file_get_content(filenameSimplPop).split("\n").toList
+  private val indexOfPop: Int = splittedSimpleFile.head.split("\t").indexOf("Pop")
+  private val zippedLists: (List[(Double, Double)], List[(Double, Double, Double)], List[(Double, Map[String, Double])])  = splittedSimpleFile.tail.map(line => {
+    val splittedLine : Array[String] = line.split("\t")
+    ((splittedLine(1).toDouble,splittedLine(2).toDouble),
+      (splittedLine(indexOfPop).toDouble,splittedLine(indexOfPop+1).toDouble,splittedLine(indexOfPop+2).toDouble),
+    (splittedLine(0).toDouble,splittedLine.drop(indexOfPop+3).sliding(2,2).map(x=> (x(0) -> x(1).toDouble)).toMap))}).unzip3
 
-  Time
-}
+  private val ListTuple :
+  ((List[Double],List[Double]),(List[Double],List[Double],List[Double]),(List[Double],List[Map[String,Double]])) =
+    (zippedLists._1.unzip,zippedLists._2.unzip3,zippedLists._3.unzip)
+
+  val TH: List[Double] = ListTuple._1._1
+  val H: List[Double] = ListTuple._1._2
+  val Pop: List[Double] = ListTuple._2._1
+  val VarPop: List[Double] = ListTuple._2._2
+  val HPop: List[Double] = ListTuple._2._3
+  val Time: List[Double] = ListTuple._3._1
+  val StateProb: List[Map[String, Double]] = ListTuple._3._2
+
+  lazy val PopStateProb : List[Map[String, Double]] = ManageInputFile.file_get_content(filenamePop).split("\n").toList.tail.
+    map(line => line.split("\t").drop(indexOfPop).sliding(2,3).map(x => (x(0) -> x(1).toDouble)).toMap)
+  }
+
+
+
+
+
