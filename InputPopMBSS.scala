21f2360e9c18bd755f025ee5fc4055223a3e1a65
gautierstoll <gautier.stoll@upmc.fr>
2022-11-29 13:38:21 +0100

    Reorganize popStateDist with option (not tested yet)

--- InputPopMBSS.scala
+++ InputPopMBSS.scala
@@ -10,64 +10,60 @@ import scala.math._
   * @param nodeList list of model nodes, necessary for constructing the set of NetStates
   *                 if nodeList is smaller than the one used for generating stateString, cell numbers are summed up
   */
-class PopNetState(val stateString: String,val nodeList : List[String]){
- val stringSetArray :  Array[(Set[String],Long)] = stateString.replaceAll("\\s*\\[|\\]\\s*","").
-   split(",").map( s => {val keyVal = s.replaceAll("\\{|\\}","").split(":")
-  if (keyVal.size < 2) (Set(""),0.toLong) else
-  (keyVal(0).split(" -- ").toSet,keyVal(1).toLong)})
- lazy val stateList : List[(NetState,Long)] = stringSetArray.
-   map(s => (new NetState(s._1,nodeList),s._2)).toList
- lazy val state : Map[NetState,Long] = stateList.groupBy(_._1).map(x => (x._1 -> x._2.map(_._2).sum))
+class PopNetState(val stateString: String,val nodeList : List[String]) {
+ val stringSetArray: Array[(Set[String], Long)] = stateString.replaceAll("\\s*\\[|\\]\\s*", "").
+   split(",").map(s => {
+  val keyVal = s.replaceAll("\\{|\\}", "").split(":")
+  if (keyVal.size < 2) (Set(""), 0.toLong) else
+   (keyVal(0).split(" -- ").toSet, keyVal(1).toLong)
+ })
+ lazy val stateList: List[(NetState, Long)] = stringSetArray.
+   map(s => (new NetState(s._1, nodeList), s._2)).toList
+ lazy val state: Map[NetState, Long] = stateList.groupBy(_._1).map(x => (x._1 -> x._2.map(_._2).sum))
 
  /** number of cells
    *
    */
  lazy val nbCell: Long = stringSetArray.map(_._2).sum
 
-  override def toString: String = stateString
+ override def toString: String = stateString
 
-  /** number of cells with active node
-    *
-    * @param node
-    * @return
-    */
-  def activeNodeNb(node : String) : Long = stringSetArray.filter(x => x._1.contains(node)).map(_._2).sum
+ /** number of cells with active node
+   *
+   * @param node
+   * @return
+   */
+ def activeNodeNb(node: String): Long = stringSetArray.filter(x => x._1.contains(node)).map(_._2).sum
 
-   // state.filterKeys(ns => ns.activeNodes.contains(node)).values.sum
-
-
-
-  /** ratio of cells with active node
-    *
-    * @param node
-    * @return
-    */
-  def activeNodeRatio(node : String) : Double = this.activeNodeNb(node).toDouble/nbCell
-
-
-   // this.activeNodeNb(node).toDouble/state.values.sum
+ /** ratio of cells with active node
+   *
+   * @param node
+   * @return
+   */
+ def activeNodeRatio(node: String): Option[Double] =
+  nbCell match {
+   case 0 => None
+   case n: Long => Some(this.activeNodeNb(node).toDouble / n)
+  }
 
  /** number of cells in a given state
    *
    * @param netState network state (can "enclose" network states of the model)
    * @return number of cells
    */
- def stateNb(netState: NetState) : Long = stringSetArray.filter(strSNb =>
+ def stateNb(netState: NetState): Long = stringSetArray.filter(strSNb =>
   (netState.activeNodes.diff(strSNb._1).isEmpty & netState.inactiveNodes.intersect(strSNb._1).isEmpty)).
    map(_._2).sum
-
-
-
-  /* state.filter(netStateNb =>
-  (netState.activeNodes.diff(netStateNb._1.activeNodes).isEmpty & netState.inactiveNodes.diff(netStateNb._1.inactiveNodes).isEmpty)).
-   values.sum */
 
  /** ratio in a given state
    *
    * @param netState network state (can "enclose" network states of the model)
    * @return ratio
    */
- def stateRatio(netState: NetState) : Double = this.stateNb(netState).toDouble/nbCell
+ def stateRatio(netState: NetState): Option[Double] = nbCell match {
+  case 0 => None
+  case n => Some(this.stateNb(netState).toDouble / n)
+ }
 }
 
 /** Sub class of BndMbss, for PopMaBoSS
@@ -124,22 +120,53 @@ private def logitSens(p:Double,sensit:Double) : Double = p match { // logit with
     })
     .values.sum
 
+ def probDist[OutType](fPopNetState : (PopNetState => Option[OutType])) : List[(OutType,Double)] = {
+  val nonNormalizedDist = pStMap.toList.map(pStProb => (fPopNetState(pStProb._1), pStProb._2)).
+    flatMap(optOutProb => optOutProb._1 match {
+     case Some(out) => Some(out, optOutProb._2)
+     case None => None
+    }).groupBy(_._1).map(x=> (x._1,x._2.map(_._2).sum)).toList
+  val normFactor = nonNormalizedDist.map(_._2).sum
+  nonNormalizedDist.map(x => (x._1,x._2/normFactor))
+ }
+
+ def probDistNb: List[(Long, Double)] = probDist(pNSt => Some(pNSt.nbCell))
+
+ def probDistNodeNb(node: String): List[(Long , Double)] = probDist(pNSt => Some(pNSt.activeNodeNb(node)))
+ def probDistNodeNb(nodes: List[String]): List[(List[Long] , Double)] =
+  probDist(pNSt => Some(nodes.map(x => pNSt.activeNodeNb(x))))
+ def probDistNodeRatio(node: String): List[(Double, Double)] = probDist(pNSt => pNSt.activeNodeRatio(node))
+ def probDistNodeRatio(nodes: List[String]): List[(List[Double], Double)] =
+  probDist(pNSt => {
+   val listOpt = nodes.map(x => pNSt.activeNodeRatio(x))
+   if (listOpt.contains(None)) {None} else Some(listOpt.flatten)})
+
+ def probDistStateNb(state: NetState): List[(Long, Double)] = probDist(pNSt => Some(pNSt.stateNb(state)))
+ def probDistStateNb(states: List[NetState]): List[(List[Long], Double)] =
+  probDist(pNSt => Some(states.map(x => pNSt.stateNb(x))))
+
+ def probDistStateRatio(state: NetState): List[(Double, Double)] = probDist(pNSt => pNSt.stateRatio(state))
+ def probDistStateRatio(states: List[NetState]): List[(List[Double], Double)] =
+  probDist(pNSt => {
+   val listOpt = states.map(x => pNSt.stateRatio(x))
+   if (listOpt.contains(None)) {None} else {Some(listOpt.flatten)}})
+
+
  /** expectation number of a network state
    *
    * @param nState network state (can "enclose" network states of the model)
    * @return expectation value
    */
  def expectNb (nState :NetState) : Double =
-  pStMap.map(pStateProb => pStateProb._1.stateNb(nState).toDouble*pStateProb._2).sum
+  probDistStateNb(nState).map(x => x._1*x._2).sum
 
  /** expectation number of an active node
    *
    * @param node node name
    * @return expectation value
    */
- def expectActiveNodeNb (node : String) : Double = {
-  this.expectNb(new NetState(Map(node -> true)))
- }
+ def expectActiveNodeNb (node : String) : Double =
+ probDistNodeNb(node).map(x => x._1*x._2).sum
 
  /** expectation log number of a network state (0 numbers are replaced by 1)
    *
@@ -147,8 +174,7 @@ private def logitSens(p:Double,sensit:Double) : Double = p match { // logit with
    * @return expectation value
    */
  def expectLogNb (nState :NetState) : Double =
-  pStMap.map(pStateProb => logNoZero(pStateProb._1.stateNb(nState))*pStateProb._2).sum
-
+  probDistStateNb(nState).map(x => logNoZero(x._1)*x._2).sum
 
  /** expectation ratio of a network state
    *
@@ -156,7 +182,7 @@ private def logitSens(p:Double,sensit:Double) : Double = p match { // logit with
    * @return expectation value
    */
  def expectRatio (nState : NetState) : Double =
-  pStMap.map(pStateProb => (pStateProb._1.stateRatio(nState))*pStateProb._2).sum
+  probDistStateRatio(nState).map(x => x._1*x._2).sum
 
  /** expectation ratio of an active node
    *
@@ -164,7 +190,7 @@ private def logitSens(p:Double,sensit:Double) : Double = p match { // logit with
    * @return expectation value
    */
  def expectActiveNodeRatio (node : String) : Double =
-  this.expectRatio(new NetState(Map(node -> true)))
+  probDistNodeRatio(node).map(x => x._1*x._2).sum
 
  /** expectation of logit ratio can provide a min value (logit(0) -> logit(min)logit(1))
    * a min value is used (logit(0) -> logit(sensit), logit(1) -> logit(1-sensit)
@@ -175,7 +201,7 @@ private def logitSens(p:Double,sensit:Double) : Double = p match { // logit with
    */
  def expectLogitRatio(nState : NetState, sensit : Double = 0): Double = {
   val sensit4Logit : Double = if (sensit == 0.0) { minSensit } else sensit
-  pStMap.map(pStateProb => logitSens(pStateProb._1.stateRatio(nState),sensit4Logit)*pStateProb._2).sum
+  probDistStateRatio(nState).map(x => logitSens(x._1,sensit4Logit)*x._2).sum
  }
 
  /** covariance number of network state pairs
@@ -184,14 +210,13 @@ private def logitSens(p:Double,sensit:Double) : Double = p match { // logit with
    * @param nState2 network state (can "enclose" network states of the model)
    * @return covariance
    */
- def covNb (nState1 : NetState, nState2 : NetState) : Double = {
+ def covNb(nState1: NetState, nState2: NetState): Double = {
   val expNbState1 = expectNb(nState1)
   val expNbState2 = expectNb(nState2)
-  pStMap.map(pStatProb =>
-   (pStatProb._1.stateNb(nState1).toDouble - expNbState1)*
-     (pStatProb._1.stateNb(nState2).toDouble - expNbState2)*
-     pStatProb._2).sum
+  probDistStateNb(List(nState1, nState2)).map(statesProb =>
+   (statesProb._1.head.toDouble - expNbState1) * (statesProb._1.tail.head.toDouble - expNbState2) * statesProb._2).sum
  }
+
  /** covariance number of active node pairs
    *
    * @param node1 node name
@@ -211,11 +236,9 @@ private def logitSens(p:Double,sensit:Double) : Double = p match { // logit with
  def covLogNb (nState1 : NetState, nState2 : NetState) : Double = {
   val expLogNbState1 = expectLogNb(nState1)
   val expLogNbState2 = expectLogNb(nState2)
-  pStMap.map(pStatProb =>
-   (logNoZero(pStatProb._1.stateNb(nState1)) - expLogNbState1)*
-     (logNoZero(pStatProb._1.stateNb(nState2)) - expLogNbState2)*
-     pStatProb._2).sum
- }
+  probDistStateNb(List(nState1, nState2)).map(statesProb =>
+   (logNoZero(statesProb._1.head.toDouble) - expLogNbState1) * (logNoZero(statesProb._1.tail.head.toDouble) - expLogNbState2) * statesProb._2).sum}
+
 
  /** covariance ratio of network state pairs
    *
@@ -226,11 +249,8 @@ private def logitSens(p:Double,sensit:Double) : Double = p match { // logit with
  def covRatio (nState1 : NetState, nState2 : NetState) : Double = {
   val expRatioState1 = expectRatio(nState1)
   val expRatioState2 = expectRatio(nState2)
-  pStMap.map(pStatProb =>
-   (pStatProb._1.stateRatio(nState1) - expRatioState1)*
-     (pStatProb._1.stateRatio(nState2) - expRatioState2)*
-     pStatProb._2).sum
- }
+  probDistStateRatio(List(nState1,nState2)).map(ratiosProb =>
+   (ratiosProb._1.head - expRatioState1)*(ratiosProb._1.tail.head-expRatioState2)*ratiosProb._2).sum}
 
  /** covariance ratio of active node pairs
    *
@@ -254,20 +274,8 @@ private def logitSens(p:Double,sensit:Double) : Double = p match { // logit with
   val sensit4Logit : Double = if (sensit == 0.0) { minSensit } else sensit
   val expLogitRatioState1 = expectLogitRatio(nState1,sensit)
   val expLogitRatioState2 = expectLogitRatio(nState2,sensit)
-  pStMap.map(pStatProb =>
-   (logitSens(pStatProb._1.stateRatio(nState1),sensit4Logit) - expLogitRatioState1)*
-     (logitSens(pStatProb._1.stateRatio(nState2),sensit4Logit) - expLogitRatioState2)*
-     pStatProb._2).sum
+  probDistStateRatio(List(nState1,nState2)).map(ratiosProb =>
+   (logitSens(ratiosProb._1.head,sensit4Logit) - expLogitRatioState1) *
+     (logitSens(ratiosProb._1.tail.head,sensit4Logit) - expLogitRatioState2)*ratiosProb._2).sum}
 
-
- }
- def probDist[OutType](fPopNetState : (PopNetState => OutType)) : List[(OutType,Double)] =
-  pStMap.toList.map(pStProb => (fPopNetState(pStProb._1),pStProb._2)).
-    groupBy(_._1).map(x=> (x._1,x._2.map(_._2).sum)).toList
-
- def probDistNb: List[(Long, Double)] = probDist(pNSt => pNSt.nbCell)
- def probDistNodeNb(node: String): List[(Long, Double)] = probDist(pNSt => pNSt.activeNodeNb(node))
- def probDistNodeRatio(node: String): List[(Double, Double)] = probDist(pNSt => pNSt.activeNodeRatio(node))
- def probDistStateNb(state: NetState): List[(Long, Double)] = probDist(pNSt => pNSt.stateNb(state))
- def probDistStateRatio(state: NetState): List[(Double, Double)] = probDist(pNSt => pNSt.stateRatio(state))
 }
