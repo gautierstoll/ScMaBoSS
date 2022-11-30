b45ebdad8b6a58fe3dd873cebd77620ef4135345
gautierstoll <gautier.stoll@upmc.fr>
2022-11-30 17:00:12 +0100

    Limited number of lines in PopMaBoSS output

--- PopResult.scala
+++ PopResult.scala
@@ -9,9 +9,11 @@ import scala.collection.immutable.List
   * @param filenamePop
   * @param filenameSimplPop
   * @param listNodes
+  * @param listLines Lines to use, header will be automatically included
   */
-class PResultFromFile(filenamePop: String,filenameSimplPop:String,val listNodes: List[String]) {
-  private val splittedSimpleFile: List[String] = ManageInputFile.file_get_content(filenameSimplPop).split("\n").toList
+class PResultFromFile(filenamePop: String,filenameSimplPop:String,val listNodes: List[String],listLines : List[Int] = List()) {
+  val listLinesHead = if (listLines.isEmpty | listLines.contains(0)) {listLines} else {0 :: listLines}
+  private val splittedSimpleFile: List[String] = ManageInputFile.file_get_content(filenameSimplPop,listLinesHead).split("\n").toList
   private val indexOfPop: Int = splittedSimpleFile.head.split("\t").indexOf("Pop")
   private val zippedLists: (List[(Double, Double)], List[(Double, Double, Double)], List[(Double, Map[String, Double])]) =
     splittedSimpleFile.tail.map(line => {
@@ -34,7 +36,7 @@ class PResultFromFile(filenamePop: String,filenameSimplPop:String,val listNodes:
   val time: List[Double] = listTuple._3._1
   val stateProb: List[Map[String, Double]] = listTuple._3._2
 
-  lazy val popStateProb: List[Map[String, Double]] = ManageInputFile.file_get_content(filenamePop).split("\n").toList.tail.
+  lazy val popStateProb: List[Map[String, Double]] = ManageInputFile.file_get_content(filenamePop,listLinesHead).split("\n").toList.tail.
     map(line => line.split("\t").drop(indexOfPop).sliding(2, 3).map(x => (x(0) -> x(1).toDouble)).toMap)
 
   /** Compute the probability to detect a node, above a minimum number of cells
@@ -56,9 +58,3 @@ class PResultFromFile(filenamePop: String,filenameSimplPop:String,val listNodes:
   def probDetectNode(node: String, minCellProp: Double): List[Double] = popStateProb.map(
     pStateDist => new popStateDist(pStateDist, listNodes).probDetectNode(node, minCellProp))
 }
-
-/** pop net state probability distribution
-  *
-  * @param inputMap map between string representing pop net state and probability
-  * @param listNodes list of nodes
-  */
